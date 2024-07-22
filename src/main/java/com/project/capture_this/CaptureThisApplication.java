6c965975add6486b13b67c9a5b9e9654f2006648
Dani <danieltraykov@gmail.com>
2024-07-22 18:36:11 +0300

    Project base

---
+++ src/main/java/com/project/capture_this/CaptureThisApplication.java
@@ -0,0 +1,13 @@
+package com.project.capture_this;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptureThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptureThisApplication.class, args);
+	}
+
+}
