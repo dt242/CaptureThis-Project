3caf849b02a7e5c70f5827044b8813dca2eab57b
Dani <danieltraykov@gmail.com>
2024-07-25 22:45:58 +0300

    Adding SecurityConfig class

---
+++ src/main/java/com/project/capture_this/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.project.capture_this.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        authorizeRequest -> {
+                            authorizeRequest
+                                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                                    .requestMatchers("/", "/users/login", "/users/login-error", "/users/register", "/about", "/contacts").permitAll()
+                                    .anyRequest().authenticated();
+                        }
+                )
+                .formLogin(
+                        formLogin -> {
+                            formLogin.loginPage("/users/login");
+                            formLogin.usernameParameter("username");
+                            formLogin.passwordParameter("password");
+                            formLogin.defaultSuccessUrl("/", true);
+                            formLogin.failureUrl("/users/login-error");
+                        }
+                )
+                .logout(
+                        logout -> {
+                            logout.logoutUrl("/users/logout");
+                            logout.logoutSuccessUrl("/");
+                            logout.invalidateHttpSession(true);
+                        }
+                )
+                // TODO make it work or demonstrate it
+//                .exceptionHandling(
+//                        exceptionHandling -> {
+//                            exceptionHandling.accessDeniedPage("/access-denied");
+//                        }
+//                )
+                .build();
+    }
+}
