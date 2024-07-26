741adf773637f352de4b636298ed289cb0bfd330
Dani <danieltraykov@gmail.com>
2024-07-26 14:21:55 +0300

    Creating AppUserDetailsService.java

---
+++ src/main/java/com/project/capture_this/service/AppUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.project.capture_this.service;
+
+import com.project.capture_this.model.entity.User;
+import com.project.capture_this.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AppUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public AppUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByUsername(username)
+                .map(this::mapToUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("User with username " + username + " not found"));
+    }
+
+    private UserDetails mapToUserDetails(User user) {
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .authorities(mapToGrantedAuthorities(user))
+                .build();
+    }
+
+    private List<GrantedAuthority> mapToGrantedAuthorities(User user) {
+        return user
+                .getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
