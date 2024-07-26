65d2e38e846de4426a3636765d53ed223735c042
Dani <danieltraykov@gmail.com>
2024-07-26 14:20:03 +0300

    Deleting Config.java (adding it to SecurityConfig.java), adding userDetailsService to SecurityConfig.java

--- src/main/java/com/project/capture_this/config/SecurityConfig.java
+++ src/main/java/com/project/capture_this/config/SecurityConfig.java
@@ -1,14 +1,31 @@
 package com.project.capture_this.config;
 
+import com.project.capture_this.service.AppUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private AppUserDetailsService userDetailsService;
+
+    public SecurityConfig(AppUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -25,20 +42,24 @@ public class SecurityConfig {
                 )
                 .formLogin(
                         formLogin -> {
-                            formLogin.loginPage("/users/login");
+                            formLogin.loginPage("/login");
+                            formLogin.defaultSuccessUrl("/home", true);
                             formLogin.usernameParameter("username");
                             formLogin.passwordParameter("password");
-                            formLogin.defaultSuccessUrl("/", true);
                             formLogin.failureUrl("/users/login-error");
                         }
                 )
                 .logout(
                         logout -> {
-                            logout.logoutUrl("/users/logout");
+                            logout.logoutUrl("/logout");
                             logout.logoutSuccessUrl("/");
                             logout.invalidateHttpSession(true);
                         }
                 )
                 .build();
+    }
+
+    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+        builder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
