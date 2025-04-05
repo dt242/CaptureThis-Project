17c4c6517024f2d6280a84a5e44846302862156e
Dani <danieltraykov@gmail.com>
2025-04-05 16:54:54 +0300

    UserRepository.java: Remove unused methods

--- src/main/java/com/project/capture_this/repository/UserRepository.java
+++ src/main/java/com/project/capture_this/repository/UserRepository.java
@@ -6,31 +6,25 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // Find a user by their username
+    @Query("SELECT u.id FROM User u")
+    List<Long> findAllUserIds();
     Optional<User> findByUsername(String username);
 
-    // Find a user by their email
     Optional<User> findByEmail(String email);
-
-    // Check if a user exists by their username
-    boolean existsByUsername(String username);
-
-    // Check if a user exists by their email
-    boolean existsByEmail(String email);
-
-//    @Query("SELECT u FROM User u LEFT JOIN FETCH u.followers LEFT JOIN FETCH u.following WHERE u.id = :userId")
-//    User findByIdWithDetails(@Param("userId") Long userId);
 
     Optional<User> findByUsernameOrEmail(String username, String email);
 
-//    @Query("SELECT u from User u WHERE CONCAT(u.firstName, u.lastName) LIKE CONCAT('%', :query, '%')")
-//    List<User> searchUsers(String query);
-
     List<User> findByFirstNameContainingOrLastNameContaining(String firstName, String lastName);
+
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.id = :userId")
+    Optional<User> findByIdWithRoles(@Param("userId") Long userId);
+
+    List<User> findByUpdatedAtBeforeAndIsActiveTrue(LocalDateTime lastActiveBefore);
 }
