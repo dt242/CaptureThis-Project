01302d1d87db15a9b050eba6b7e31279435b4e6f
Dani <danieltraykov@gmail.com>
2025-04-05 16:48:21 +0300

    PostRepository.java: Remove unused methods

--- src/main/java/com/project/capture_this/repository/PostRepository.java
+++ src/main/java/com/project/capture_this/repository/PostRepository.java
@@ -1,6 +1,5 @@
 package com.project.capture_this.repository;
 
-import com.project.capture_this.model.entity.Comment;
 import com.project.capture_this.model.entity.Post;
 import com.project.capture_this.model.entity.User;
 import com.project.capture_this.model.enums.PostStatus;
@@ -8,30 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    // Find posts by the user
     List<Post> findByUserAndStatus(User user, PostStatus status);
     List<Post> findByUser(User user);
-
-    // Find a post by its title
-    Optional<Post> findByTitle(String title);
-
-    // Find all posts with a specific status
-    List<Post> findByStatus(PostStatus status);
-
-    // Find posts with a specific description (could use like query or exact match depending on requirements)
-    List<Post> findByDescriptionContaining(String description);
-
-    // Find all posts that have a specific comment
-    List<Post> findByCommentsContaining(Comment comment);
-
-    // Find all posts liked by a specific user
-    List<Post> findByLikesUser(User user);
-
-    // Find all posts marked as favorites by a specific user
-    List<Post> findByFavoritesUser(User user);
 }
