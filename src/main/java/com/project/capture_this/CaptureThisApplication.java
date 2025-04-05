8577846031849feb969f96f9f47fb6bd6cd2076a
Dani <danieltraykov@gmail.com>
2025-04-05 13:32:05 +0300

    Add EnableScheduling annotation to CaptureThisApplication

--- src/main/java/com/project/capture_this/CaptureThisApplication.java
+++ src/main/java/com/project/capture_this/CaptureThisApplication.java
@@ -2,8 +2,10 @@ package com.project.capture_this;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CaptureThisApplication {
 
 	public static void main(String[] args) {
