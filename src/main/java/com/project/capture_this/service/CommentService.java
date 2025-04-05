ae32a82bae6d3b9753afa13310b30f8842c52ef7
Dani <danieltraykov@gmail.com>
2025-04-05 16:42:05 +0300

    Convert from RestTemplate to RestClient

--- src/main/java/com/project/capture_this/service/CommentService.java
+++ src/main/java/com/project/capture_this/service/CommentService.java
@@ -1,51 +1,48 @@
 package com.project.capture_this.service;
 
 import com.project.capture_this.model.dto.CommentDTO;
-import com.project.capture_this.model.entity.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
+import java.util.Arrays;
 
 @Service
 public class CommentService {
 
-    public static CommentDTO mapToCommentDTO(Comment comment) {
-        return CommentDTO.builder()
-                .id(comment.getId())
-                .postId(comment.getPost().getId())
-                .userId(comment.getUser().getId())
-                .content(comment.getContent())
-                .createdAt(comment.getCreatedAt())
-                .updatedAt(comment.getUpdatedAt())
-                .isDeleted(comment.isDeleted())
-                .build();
-    }
-
-    @Autowired
-    private RestTemplate restTemplate;
+    private final RestClient restClient;
 
     private final String COMMENTS_SERVICE_URL = "http://localhost:8081/api/comments";
 
+    @Autowired
+    public CommentService(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.baseUrl(COMMENTS_SERVICE_URL).build();
+    }
+
     public List<CommentDTO> getCommentsByPostId(Long postId) {
-        String url = COMMENTS_SERVICE_URL + "/post/" + postId;
-        CommentDTO[] comments = restTemplate.getForObject(url, CommentDTO[].class);
-        return List.of(comments);
+        CommentDTO[] comments = restClient
+                .get()
+                .uri("/post/{postId}", postId)
+                .retrieve()
+                .body(CommentDTO[].class);
+
+        return Arrays.asList(comments);
     }
 
     public CommentDTO addComment(CommentDTO commentDTO) {
-        return restTemplate.postForObject(COMMENTS_SERVICE_URL, commentDTO, CommentDTO.class);
-    }
-
-    public CommentDTO updateComment(Long commentId, String content) {
-        String url = COMMENTS_SERVICE_URL + "/" + commentId;
-        restTemplate.put(url, content);
-        return restTemplate.getForObject(url, CommentDTO.class);
+        return restClient
+                .post()
+                .uri("")
+                .body(commentDTO)
+                .retrieve()
+                .body(CommentDTO.class);
     }
 
     public void deleteComment(Long commentId) {
-        String url = COMMENTS_SERVICE_URL + "/" + commentId;
-        restTemplate.delete(url);
+        restClient
+                .delete()
+                .uri("/{commentId}", commentId)
+                .retrieve();
     }
 }
