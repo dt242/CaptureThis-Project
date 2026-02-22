e0d5d63ec5728160d9d7cc3c134a3e435a7e4134
Dani <danieltraykov@gmail.com>
2026-02-22 13:32:23 +0200

    refactor: remove redundant @Autowired annotation

--- src/main/java/com/project/capture_this/config/GlobalModelAttributes.java
+++ src/main/java/com/project/capture_this/config/GlobalModelAttributes.java
@@ -4,7 +4,6 @@ import com.project.capture_this.model.entity.User;
 import com.project.capture_this.service.NotificationService;
 import com.project.capture_this.service.UserService;
 import com.project.capture_this.util.SecurityUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.ui.Model;
@@ -15,7 +14,6 @@ public class GlobalModelAttributes {
     private final UserService userService;
     private final NotificationService notificationService;
 
-    @Autowired
     public GlobalModelAttributes(UserService userService, NotificationService notificationService) {
         this.userService = userService;
         this.notificationService = notificationService;
