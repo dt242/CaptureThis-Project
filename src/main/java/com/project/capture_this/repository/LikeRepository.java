c34cda299145035bc5f82e55c5da8577c130f758
Dani <danieltraykov@gmail.com>
2026-02-23 17:28:41 +0200

    refactor(repository): remove redundant findById method from LikeRepository

--- src/main/java/com/project/capture_this/repository/LikeRepository.java
+++ src/main/java/com/project/capture_this/repository/LikeRepository.java
@@ -5,11 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
-    Optional<Like> findById(Long id);
 
     boolean existsByPostIdAndUserId(Long postId, Long userId);
     void deleteByPostIdAndUserId(Long postId, Long userId);
